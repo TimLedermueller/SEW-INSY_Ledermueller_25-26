92a01cad4e368162fa44665360802009d7d1b5bb
TimLedermueller <t.ledermueller@htlkrems.at>
2025-09-23 14:36:13 +0200

    Zug fährt mit Tast Space und verschwindet wieder die größe ist random

--- ParallelProgramming/TrainSim/Program.cs
+++ ParallelProgramming/TrainSim/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Threading;
+using System.Collections.Generic;  
 
 
 
@@ -15,24 +16,55 @@ Console.WriteLine($"Train nr {Nr}({Thread.CurrentThread.GetHashCode()}) entering
    
         string gleis = "============================================================="; 
         Random random = new Random();
-        
-        int zugLaenge = random.Next(3, 9);
-        string zug = new string('|', zugLaenge);
+        var zuege = new List<(int pos, int len )>();
 
-        for (int pos = 0; pos <= gleis.Length; pos++)
+        while (true)
         {
-            Console.Clear();
+
+            while (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo taste = Console.ReadKey(intercept: true);
+
+                if (taste.Key == ConsoleKey.Escape)
+                {
+                    Console.CursorVisible = true;
+                    Console.Clear();
+                    return;
+                }
+
+                if (taste.Key == ConsoleKey.Spacebar)
+                {
+                    int zugLaenge = random.Next(3, 9);
+                    zuege.Add((pos: -zugLaenge, len: zugLaenge));
+                }
+
+            }
+
+            for (int i = 0; i < zuege.Count; i++)
+                zuege[i] = (zuege[i].pos + 1, zuege[i].len);
             
-            string vorne = new string('=', Math.Max(0, pos));
+            zuege.RemoveAll(z => z.pos - z.len > gleis.Length);
+            
+            char[] zeile = new char[gleis.Length];
+            for (int i = 0; i < zeile.Length; i++) zeile[i] = '=';
+            
+       
+            foreach (var z in zuege)
+            {
+                int head = z.pos;
+                int tail = head - z.len + 1;
+                for (int x = tail; x <= head; x++)
+                    if (x >= 0 && x < zeile.Length) zeile[x] = '|';
+            }
 
-           
-            int rest = Math.Max(0, gleis.Length - pos - zugLaenge);
-            string hinten = new string('=', rest);
 
-           
-            Console.WriteLine(vorne + zug + hinten);
-
-            Thread.Sleep(200);
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine(new string(zeile).PadRight(gleis.Length));
+            
+            Thread.Sleep(100);
         }
+        
+
+  
     
 
---
+++ ParallelProgramming/TrainSim/Train.cs
@@ -0,0 +1,23 @@
+﻿namespace TrainSim;
+
+public class Train
+{
+    public int Position;
+    public int Length;
+
+    public Train(int length)
+    {
+        Length = length;
+        Position = -length;
+    }
+
+    public void Step()
+    {
+        Position++;
+    }
+
+    public bool IsOutOfTrack(int trackLength)
+    {
+        return Position - Length > trackLength;
+    }
+}
