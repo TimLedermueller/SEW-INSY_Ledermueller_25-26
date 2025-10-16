d3496f0312815e40e3075a320b19e55c9d25dcbe
TimLedermueller <t.ledermueller@htlkrems.at>
2025-10-16 14:31:58 +0200

    Unionn vs UNION ALL
    Ergebnis:
    UNION: 3079,14 ms
    UNION ALL: 1160,18 ms

--- obj/Debug/net9.0/PerformentstestUNION.AssemblyInfo.cs
+++ obj/Debug/net9.0/PerformentstestUNION.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PerformentstestUNION")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6248df6acceec90073436b8fd41d48c31c3bb0f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PerformentstestUNION")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PerformentstestUNION")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
