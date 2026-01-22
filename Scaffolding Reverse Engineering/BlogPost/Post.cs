85108675bdc2c054d68fc7296a8d96a79b043de4
TimLedermueller <t.ledermueller@htlkrems.at>
2026-01-22 12:03:30 +0100

    Comit

---
+++ Scaffolding Reverse Engineering/BlogPost/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogPost;
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    
+    public int BlogId { get; set; } //Foreigen Key
+    public Blog Blog { get; set; }
+}
