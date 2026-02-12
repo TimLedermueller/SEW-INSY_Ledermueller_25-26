99f4c9fff48432edb6c6c1f33a7b21b9ccafd3cf
TimLedermueller <t.ledermueller@htlkrems.at>
2026-02-12 10:54:02 +0100

    Commit

---
+++ DatenbankundEntityFW/Model/Entities/Demodatum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Datenbank_und_Entity_FW.Entities;
+
+public partial class Demodatum
+{
+    public int Id { get; set; }
+
+    public string? Value { get; set; }
+}
