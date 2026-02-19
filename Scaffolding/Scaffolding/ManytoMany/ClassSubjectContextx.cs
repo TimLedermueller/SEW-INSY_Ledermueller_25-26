b9031d2923081ada3060fc79085670a73be83537
TimLedermueller <t.ledermueller@htlkrems.at>
2026-02-19 12:01:20 +0100

    änderung

--- Scaffolding/Scaffolding/ManytoMany/ClassSubjectContextx.cs
+++ Scaffolding/Scaffolding/ManytoMany/ClassSubjectContextx.cs
@@ -30,6 +30,39 @@ public class ClassSubjContextx : DbContext
             .HasOne(x => x.Subject)
             .WithMany(s => s.ClassSubjects)
             .HasForeignKey(x => x.SubjectId);
+        
+        //DataSeeding
+        
+        
+        modelBuilder.Entity<Classes>().HasData(
+            new Classes { Id = 1, Name = "4C" },
+            new Classes { Id = 2, Name = "1C" },
+            new Classes { Id = 3, Name = "2C" }
+        );
+
+        modelBuilder.Entity<Subject>().HasData(
+            new Subject { Id = 1, Title = "German" },
+            new Subject { Id = 2, Title = "Mathematics" },
+            new Subject { Id = 3, Title = "English" },
+            new Subject { Id = 4, Title = "Physics" },
+            new Subject { Id = 5, Title = "Programming" }
+        );
+        
+        modelBuilder.Entity<ClassSubjectContent>().HasData(
+            // 4C
+            new ClassSubjectContent { ClassId = 1, SubjectId = 1, Content = "Meinungsrede" },
+            new ClassSubjectContent { ClassId = 1, SubjectId = 5, Content = "C#, OOP, APIs" },
+            new ClassSubjectContent { ClassId = 1, SubjectId = 2, Content = "Vectors, integrals" },
+
+            // 1C
+            new ClassSubjectContent { ClassId = 2, SubjectId = 5, Content = "Basics, algorithms" },
+            new ClassSubjectContent { ClassId = 2, SubjectId = 3, Content = "Grammar + vocabulary" },
+
+            // 2C
+            new ClassSubjectContent { ClassId = 3, SubjectId = 2, Content = "Equations + functions" },
+            new ClassSubjectContent { ClassId = 3, SubjectId = 4, Content = "Mechanics basics" }
+        );
+        
     }
     
     
