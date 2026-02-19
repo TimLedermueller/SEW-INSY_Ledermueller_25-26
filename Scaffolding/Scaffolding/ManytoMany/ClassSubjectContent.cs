0fad5664625f34963baa7f7f5be58770413d0c90
TimLedermueller <t.ledermueller@htlkrems.at>
2026-02-19 11:35:36 +0100

    RELATIONSHIPS und kleine Namensänderungen

--- Scaffolding/Scaffolding/ManytoMany/ClassSubjectContent.cs
+++ Scaffolding/Scaffolding/ManytoMany/ClassSubjectContent.cs
@@ -1,6 +1,17 @@
 ﻿namespace ManytoMany;
 
+
 public class ClassSubjectContent
 {
-    
+    public int ClassId { get; set; }
+    public int SubjectId { get; set; }
+
+    public string Content { get; set; } = string.Empty;
+
+    public Classes? Class { get; set; }
+    public Subject? Subject { get; set; }
 }
+
+
+
+
