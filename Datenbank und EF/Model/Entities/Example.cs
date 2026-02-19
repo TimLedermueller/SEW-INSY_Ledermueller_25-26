1607ed71cbe7753d85e79649e2bf23b708ae167d
TimLedermueller <t.ledermueller@htlkrems.at>
2026-02-19 10:48:49 +0100

    erweierung

---
+++ Datenbank und EF/Model/Entities/Example.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema; 
+namespace Model.Entities;
+
+
+[Table("Example")]
+
+public class Example :IHasId
+{
+    public int Id { get; set; }
+    public int Value1 { get; set; }
+    public int Value2 { get; set; }
+}
---
+++ Datenbank und EF/Model/Entities/IHasId.cs
@@ -0,0 +1,6 @@
+﻿namespace Model.Entities;
+
+public interface IHasId
+{
+    public int Id { get; }
+}
